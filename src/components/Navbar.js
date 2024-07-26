b179b1911fd2a8ad6a05d625c6a0f614f0623125
Rolly Cortez <cortezrc6@gmail.com>
2024-07-27 04:56:05 +0800

    babyran navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import { Component } from "react";
+import "./NavbarStyles.css";
+class Navbar extends Component {
+    state = {clicked: false};
+    handleClick = () =>{
+        this.setState({clicked: !this.state.clicked});
+    }
+  render(){  
+return (
+    <><nav>
+        <a><img src="./babyran images/Babyran Revamp logo1.png"/></a>
+        <div>
+        <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
+        <li><a className="active" href="index.html">Home</a></li>
+        <li><a href="#">Download</a></li>
+        <li><a href="#">Top-Up</a></li>
+        <li><a href="#">Community</a></li>
+            
+        </ul>
+
+        </div>
+        <div id="mobile" onClick={this.handleClick}>
+        <i id="bar" className={this.state.clicked ?
+            "fas fa-times" : "fas fa-bars"}></i>
+            </div>
+        
+        </nav>
+        </>
+)
+}
+}
+export default Navbar;
