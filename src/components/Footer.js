1ce52e182c5905669ff6cf4b2775d354dd3f2baa
Rolly Cortez <cortezrc6@gmail.com>
2024-08-02 15:33:54 +0800

    Project Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
         <>
         <div className="Footer-container">
             <div className="Footer-left">
-        <img src="./babyran images/Babyran Revamp logo1.png"/>
+        <img src="./babyran images/Babyran Revamp logo1.png" alt="Babyran Logo"/>
             <div className="Footer-content">
         <h1>BABYRAN REVAMP</h1>
         <p>Copyright 2024 Babyran Revamp, All Rights Reserved.</p>
@@ -13,7 +13,7 @@ function Footer(){
         </div>
 
         <div className="rc-development">
-        <img src="./babyran images/RC.png"/>
+        <img src="./babyran images/RC.png" alt="Babyran Logo"/>
             <p>Developed by: Rolly Cortez</p>
         </div>
         </div>
--- src/components/ServerInfo.js
+++ src/components/ServerInfo.js
@@ -3,7 +3,7 @@ function ServerInfo(){
     return(
         <>
         <div className="server-container">
-        <img src="./babyran images/Babyran Revamp logo1.png"/>
+        <img src="./babyran images/Babyran Revamp logo1.png" alt="Babyran Logo"/>
         <div className="server-description">
             <div className="server-info">
             <h1>SERVER INFORMATION</h1>
